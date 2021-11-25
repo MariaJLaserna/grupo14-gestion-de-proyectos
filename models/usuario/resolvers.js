f5018e561b619288a929011c60adb65c4c16fa68
Mariajlaserna <mariajlaserna@gmail.com>
2021-11-25 10:04:50 -0500

    se modifica rol de usuario porque no se debe editar

--- models/usuario/resolvers.js
+++ models/usuario/resolvers.js
@@ -34,7 +34,6 @@ const resolversUsuario = {
         apellido: args.apellido,
         identificacion: args.identificacion,
         correo: args.correo,
-        rol: args.rol,
         estado: args.estado,
       },
       {new: true}
