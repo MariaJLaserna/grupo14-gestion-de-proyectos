6a4312a8e03a35b5649d9ead5dfd31376eacfcb6
Mariajlaserna <mariajlaserna@gmail.com>
2021-12-02 20:42:13 -0500

    cambio

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { UserContext } from 'context/userContext';
 import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import Index from 'pages/Index';
-import Page2 from 'pages/Page2';
+// import Page2 from 'pages/Page2';
 import IndexCategory1 from 'pages/category1/Index';
 import Category1 from 'pages/category1/CategoryPage1';
 import IndexUsuarios from 'pages/usuarios';
@@ -79,7 +79,7 @@ function App() {
                 <Route path='' element={<Index />} />
                 <Route path='/usuarios' element={<IndexUsuarios />} />
                 <Route path='/usuarios/editar/:_id' element={<EditarUsuario />} />
-                <Route path='page2' element={<Page2 />} />
+                {/* <Route path='page2' element={<Page2 />} /> */}
                 <Route path='category1' element={<IndexCategory1 />} />
                 <Route path='category1/page1' element={<Category1 />} />
               </Route>
