e5d9a942c9f5eff8985a7eaa696c776f545bee63
Mariajlaserna <mariajlaserna@gmail.com>
2021-12-07 08:31:25 -0500

    confirmar cambio

--- back-end/db/db.js
+++ back-end/db/db.js
@@ -22,14 +22,3 @@ export default conectarBD;
 
 // const url = "mongodb://mintic:MinticEquipo14@gestionproyectosmisiont-shard-00-00.b7eel.mongodb.net:27017,gestionproyectosmisiont-shard-00-01.b7eel.mongodb.net:27017,gestionproyectosmisiont-shard-00-02.b7eel.mongodb.net:27017/GestionProyectos?ssl=true&replicaSet=atlas-be29yz-shard-0&authSource=admin&retryWrites=true&w=majority";
 
-// const conectarDB = async () =>{
-//     return await connect(
-//         url
-//         ,{ useNewUrlParser: true,useUnifiedTopology: true }).then(() => {
-//             console.log('Conexion exitosa');
-//         }).catch(e => {
-//             console.error("Error conectando a la bd", e)
-//         });
-// }
-
-// export default conectarBD;
